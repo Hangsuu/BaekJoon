4736fdba15c1862b3633685e7d4e30c1a26bb87a
westh <westh@Gram>
2023-01-29 21:46:14 +0900

    배열 평균, 중간값

---
+++ baekjoon/src/step/array2/Array25305.java
@@ -0,0 +1,26 @@
+package step.array2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Array25305 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int[] arr = new int[5];
+		int sum=0;
+		for(int i=0; i<5; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum+=arr[i];
+		}
+		Arrays.sort(arr);
+		bw.write((sum/5)+"\n");
+		bw.write(arr[2]+"");
+		br.close();
+		bw.close();
+	}
+}
