8ba8e863595d69a9589c035ee6955a4932c731db
westh <westh@Gram>
2023-01-30 21:40:08 +0900

    숫자 쪼개서 정렬 후 출력

---
+++ baekjoon/src/step/array2/Array1427.java
@@ -0,0 +1,20 @@
+package step.array2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Array1427 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] s = br.readLine().split("");
+		Arrays.sort(s);
+		for(int i=s.length-1; i>=0; i--) bw.write(s[i]);
+		bw.close();
+		br.close();
+	}
+}
