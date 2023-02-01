1e982c8c484f0ab10a54dcfcd156d3edf03836c7
Hangsuu <ohhangsu@naver.com>
2023-02-01 13:41:24 +0900

    재귀함수-Palindrome(문자열 대칭 검사)

---
+++ baekjoon/src/step/function/Function25501.java
@@ -0,0 +1,34 @@
+package step.function;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Function25501 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuffer sb = new StringBuffer();
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0; i<n; i++) {
+			String s = br.readLine();
+			sb.append(isPalindrome(s)+"\n");
+		}
+		br.close();
+		bw.write(sb.toString());
+		bw.close();
+	}
+	
+    public static String recursion(String s, int l, int r){
+        if(l >= r) return "1 "+(l+1);
+        else if(s.charAt(l) != s.charAt(r)) return "0 "+(l+1);
+        else return recursion(s, l+1, r-1);
+    }
+    
+	
+    public static String isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+}
