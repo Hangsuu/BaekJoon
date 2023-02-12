e734fc6a9a166cacc21e6de87c57c4d669177fd6
westh <westh@Gram>
2023-02-12 10:44:02 +0900

    숫자 쪼개기

---
+++ baekjoon/src/step/set/Set11478.java
@@ -0,0 +1,26 @@
+package step.set;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Set11478 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String s = br.readLine();
+		Set<String> set = new HashSet<>();
+		for(int i=0; i<s.length(); i++) {
+			for(int j=i+1; j<=s.length(); j++) {
+				set.add(s.substring(i, j));
+			}
+		}
+		bw.write(set.size()+"");
+		bw.close();
+		br.close();
+	}
+}
