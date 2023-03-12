e0a028bf082c250a06114428e5cc1b3d36eaae90
hangsuu <ohhangsu@naver.com>
2023-03-12 13:00:05 +0900

    그리디 알고리즘 최소시간 구하기

---
+++ baekjoon/src/step/greedy/Greedy11399.java
@@ -0,0 +1,30 @@
+package step.greedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Greedy11399 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int sum=0;
+		for(int i=0; i<n; i++) {
+			sum+=arr[i]*(n-i);
+		}
+		bw.write(sum+"");
+		bw.close();
+		br.close();
+	}
+}
