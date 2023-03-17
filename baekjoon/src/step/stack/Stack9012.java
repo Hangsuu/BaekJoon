8340ec1d90554df1ca453ced6400dade5d1ba7ed
hangsuu <ohhangsu@naver.com>
2023-03-17 22:06:20 +0900

    스택 ()

--- baekjoon/src/step/stack/Stack9012.java
+++ baekjoon/src/step/stack/Stack9012.java
@@ -5,33 +5,31 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Stack9012 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		Stack<Integer> stack = new Stack<>();
 		int n = Integer.parseInt(br.readLine());
 		for(int i=0; i<n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			while(st.hasMoreElements()) {
-				String temp = st.nextToken();
+			int m=0;
+			String[] s = br.readLine().split("");
+			for(int j=0; j<s.length; j++) {
+				String temp = s[j];
 				if(temp.equals("(")){
-					stack.push(1);
+					m++;
 				}
-				else {
-					if(stack.size()==0) {
-						stack.push(1);
-						break;
+				else{
+					if(m>0) {
+						m--;
 					}
 					else {
-						stack.pop();
+						m++;
+						j=s.length;
 					}
 				}
 			}
-			if(stack.size()==0) {
+			if(m==0) {
 				bw.write("YES\n");
 			}
 			else {
