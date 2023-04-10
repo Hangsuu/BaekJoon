db350964b7de3d12637ad0031f28fe1eeba93738
Hangsuu <ohhangsu@naver.com>
2023-04-10 15:16:20 +0900

    시간

---
+++ baekjoon/src/step/time/Time24265.java
@@ -0,0 +1,18 @@
+package step.time;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Time24265 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long n = Long.parseLong(br.readLine());
+		bw.write((long)n*(n-1)/2+"\n2");
+		bw.close();
+		br.close();
+	}
+}
