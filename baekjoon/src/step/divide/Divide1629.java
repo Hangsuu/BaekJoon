67ec9407451fbf70419ac748523ef0d421cf5866
hangsuu <ohhangsu@naver.com>
2023-04-18 20:23:54 +0900

    거듭제곱 나머지 구하기

--- baekjoon/src/step/divide/Divide1629.java
+++ baekjoon/src/step/divide/Divide1629.java
@@ -1,23 +1,21 @@
 package step.divide;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Divide1629 {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String[] s = br.readLine().split(" ");
-		long a = Long.parseLong(s[0]);
-		long n = Long.parseLong(s[1]);
-		long c = Long.parseLong(s[2]);
+		long a = sc.nextLong();
+		long n = sc.nextLong();
+		long c = sc.nextLong();
 		
 		bw.write(rest(a, n, c)+"");
 		bw.close();
-		br.close();
+		sc.close();
 	}
 	public static long rest(long a, long n, long c) {
 		if(n==0L) {
