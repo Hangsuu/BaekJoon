080e96561850db12f0190a7a001f9a2d16cc6bb1
hangsuu <ohhangsu@naver.com>
2023-04-23 10:44:33 +0900

    피라미드 둘레 길이

---
+++ baekjoon/src/step/math/Math15894.java
@@ -0,0 +1,18 @@
+package step.math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Math15894 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long n = Long.parseLong(br.readLine());
+		bw.write(n*4L+"");
+		bw.close();
+		br.close();
+	}
+}
