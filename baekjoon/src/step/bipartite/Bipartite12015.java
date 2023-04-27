4c0133f2690885a94090b87f7b8ca56d4d539746
hangsuu <ohhangsu@naver.com>
2023-04-27 23:00:03 +0900

    가장긴부분증가하는수열(LIS) 이분탐색 : 수열 바꿔끼우기, 이분탐색으로 바꿔끼울 공간 찾음

--- baekjoon/src/step/bipartite/Bipartite12015.java
+++ baekjoon/src/step/bipartite/Bipartite12015.java
@@ -5,22 +5,39 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Bipartite12015 {
-	private static int[] arr;
-	private static int n;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		arr = new int[n+1];
-		for(int i=1; i<=n; i++) {
-			arr[n]=Integer.parseInt(st.nextToken());
+		int[] arr = new int[n+1];
+		int count=0;
+		for(int i=0; i<n; i++) {
+			int temp = Integer.parseInt(st.nextToken());
+			if(arr[count]<temp) {
+				count++;
+				arr[count]=temp;
+			}
+			else {
+				int min=1;
+				int max=count;
+				while(min<max) {
+					int mid = (min+max)/2;
+					if(arr[mid]<temp) {
+						min=mid+1;
+					}
+					else {
+						max=mid;
+					}
+					
+				}
+				arr[min] = temp;
+			}
 		}
-		
+		bw.write(count+"");
 		
 		bw.close();
 		br.close();
