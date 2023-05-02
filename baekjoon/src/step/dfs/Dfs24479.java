21dfc2eeaf75cc167e8198b64719a7eedb6edea5
hangsuu <ohhangsu@naver.com>
2023-05-02 21:47:29 +0900

    깊이우선탐색 방문 순서

--- baekjoon/src/step/dfs/Dfs24479.java
+++ baekjoon/src/step/dfs/Dfs24479.java
@@ -5,11 +5,13 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Dfs24479 {
-	private static int[] count;
-	private static int[] index;
-	private static int[][] dfs;
+	private static int index=2;
+	private static int[] ans;
+	private static ArrayList<ArrayList<Integer>> dfs = new ArrayList<>();;
 	private static int n;
 	private static int m;
 	private static int r;
@@ -21,36 +23,35 @@ public class Dfs24479 {
 		n = Integer.parseInt(s[0]);
 		m = Integer.parseInt(s[1]);
 		r = Integer.parseInt(s[2]);
-		count = new int[n+1];
-		index = new int[m];
-		dfs = new int[n+1][m];
+		ans = new int[n+1];
+		for(int i=0; i<=n; i++) {
+			dfs.add(new ArrayList<>());
+		}
 		for(int t=0; t<m; t++) {
 			String[] s2 = br.readLine().split(" ");
 			int x = Integer.parseInt(s2[0]);
 			int y = Integer.parseInt(s2[1]);
-			dfs[x][index[x]] = y;
-			index[x]++;
+			dfs.get(x).add(y);
+			dfs.get(y).add(x);
 		}
+		for(int i=1; i<=n; i++) {
+			Collections.sort(dfs.get(i));
+		}
+		ans[r]=1;
 		dfs(r);
 		for(int i=1; i<=n; i++) {
-			bw.write(count[i]+"\n");
+			bw.write(ans[i]+"\n");
 		}
 		bw.close();
 		br.close();
 	}
 	private static void dfs(int k) {
-		for(int i=0; i<m; i++) {
-			if(dfs[k][i]==0) {
-				i=m;
-			}
-			else {
-				if(count[dfs[k][i]]==0) {
-					count[dfs[k][i]] = count[k]+1;
-				}
-				else {
-					count[dfs[k][i]] = Math.min(count[k]+1, count[dfs[k][i]]);
-				}
-				dfs(dfs[k][i]);
+		for(int i=0; i<dfs.get(k).size(); i++) {
+			int temp = dfs.get(k).get(i);
+			if(ans[temp]==0) {
+				ans[temp]=index;
+				index++;
+				dfs(temp);
 			}
 		}
 	}
