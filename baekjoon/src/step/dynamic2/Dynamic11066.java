8af5ba556b3ec03ef57a0467c5a833414d86fc59
hangsuu <ohhangsu@naver.com>
2023-06-25 20:08:22 +0900

    동적계획 파일 합치기

--- baekjoon/src/step/dynamic2/Dynamic11066.java
+++ baekjoon/src/step/dynamic2/Dynamic11066.java
@@ -5,8 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Dynamic11066 {
@@ -18,15 +16,25 @@ public class Dynamic11066 {
 		for(int k=0; k<t; k++) {
 			int n = Integer.parseInt(br.readLine());
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] arr = new int[n];
-			int[][] dp = new int[n][n];
-
+			int[][] arr = new int[n][n];
+			int[] sum = new int[n+1];
 			for(int i=0; i<n; i++) {
 				int temp = Integer.parseInt(st.nextToken());
-				arr[i] = temp;
-				dp[i][i] = temp;
+				sum[i+1]=sum[i]+temp;
 			}
-			
+			for(int i=1; i<=n; i++) {
+				for(int j=0; j<n-i; j++) {
+					for(int l=j; l<j+i; l++) {
+						if(arr[j][j+i]==0) {
+							arr[j][j+i]=arr[j][l]+arr[l+1][j+i]+sum[j+i+1]-sum[j];
+						}
+						else {
+							arr[j][j+i]=Math.min(arr[j][j+i], arr[j][l]+arr[l+1][j+i]+sum[j+i+1]-sum[j]);
+						}
+					}
+				}
+			}
+			bw.write(arr[0][n-1]+"\n");
 		}
 		bw.close();
 		br.close();
